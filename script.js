f975f9fe62edf068a044622b46631315a5ccd0b4
Ariqena <syrkinaarina407@gmail.com>
2026-04-19 14:05:07 +0900

    Remove test log from burger click event
    Removed unnecessary console log statement.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const burger = document.getElementById("burger");
 const menu = document.getElementById("menu");
 burger.addEventListener("click", () => {
-  
-  console.log('test')
   menu.classList.toggle("active");
 });
